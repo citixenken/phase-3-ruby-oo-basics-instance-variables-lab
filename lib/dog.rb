898851072b217d033ce14db96d28915dbf9dfdfa
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-16 23:02:50 +0300

    commit tree d1fd257bd5b1d33fc2d37f604f0096b3dd3e5af8

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
+    #setter method
+ def name=(dog_name)
+    @this_dogs_name = dog_name
 
+    #assign the @name instance variable a value of whatever is passed as argument
+    # @name = value
+ end
+
+ #getter method
+ def name
+    @this_dogs_name
+    #access data from the @name instance variable and return it
+    # @name
+ end
 end
+
+lassie = Dog.new #create
+lassie.name = "Lassie" #assign
+
+puts lassie.name #return
+
+# odie = Dog.new
+# odie.name = "Odie"
+
+# puts odie.name
